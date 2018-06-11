ebad4b094d1a0a156e3dd34e88c05086e379d7dc
Arne Mæhlum <arne.mahlum@gmail.com>
2018-06-11 16:10:46 +0200

    WIP: FIrebase

--- client/src/components/PageContent/pollStore.js
+++ client/src/components/PageContent/pollStore.js
@@ -1,7 +1,9 @@
 import { Listenable } from 'pockito';
 import REQ from 'util/REQ';
+import firebase from 'firebase';
 
 const uuid = poll => `${poll._id}.${poll._rev}`;
+const idByKey = snap => Object.keys(snap).map(key => ({ id: key, ...snap[key] }));
 
 const store = new Listenable({
   // uniValidator: shape({
@@ -34,11 +36,68 @@ export function getStateFor(poll) {
 // Async actions
 export function syncSubmittedAnswers(poll) {
   console.log(`sync:`, uuid(poll)); // DEBUG
+
+  const update = (obj) => store.set({
+    [uuid(poll)]: {
+      ...store[uuid(poll)],
+      prevAnswersAsync: obj
+    }
+  });
+
+  update({ req: REQ.PENDING });
+
+  firebase.database
+    .ref('polls')
+    .child(poll._id)
+    .on('value', (snap) => {
+      const allAnswers = idByKey(snap.val() || {});
+      const aggregated =
+        allAnswers
+          .filter(ans => ans._rev === poll._rev)
+          .reduce((map, ans) => {
+            if (map[ans.text]) {
+              map[ans.text]++;
+            } else {
+              map[ans.text] = 1;
+            }
+            return map;
+          }, {});
+
+      update({
+        req: REQ.SUCCESS,
+        answerMap: aggregated,
+      });
+    });
+    // TODO REQ.ERROR ?
 }
 export function unSyncSubmittedAnswers(poll) {
   console.log(`unsync:`, uuid(poll)); // DEBUG
+
+  firebase.database
+    .ref('polls')
+    .child(poll._id)
+    .off('value');
 }
 
 export function submitAnswer(poll, answer) {
   console.log(`submit:`, uuid(poll), answer); // DEBUG
+
+  const update = (obj) => store.set({
+    [uuid(poll)]: {
+      ...store[uuid(poll)],
+      prevAnswersAsync: obj
+    }
+  });
+
+  firebase.database
+    .ref('polls')
+    .child(poll._id)
+    .push({
+      _rev: poll._rev,
+      answer: answer
+    })
+    .then(
+      () => update({ req: REQ.SUCCESS }),
+      () => update({ req: REQ.ERROR }),
+    );
 }
---
+++ client/src/firebaseInit.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCoQaXKPZlGCYUXOBQ9ZwMi7coqr5Jjh8U",
+  authDomain: "kombinertmobilitet.firebaseapp.com",
+  databaseURL: "https://kombinertmobilitet.firebaseio.com",
+  projectId: "kombinertmobilitet",
+  storageBucket: "kombinertmobilitet.appspot.com",
+  messagingSenderId: "737323752552"
+});
