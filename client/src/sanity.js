3a3a89c7a8b11a194638de9f3ac99ffc60e43cd2
Arne Mæhlum <arne.mahlum@gmail.com>
2018-06-12 16:38:26 +0200

    TMP: Use dev Sanity

--- client/src/sanity.js
+++ client/src/sanity.js
@@ -3,7 +3,7 @@ import imageUrlBuilder from '@sanity/image-url';
 
 const client = sanityClient({
   projectId: '19k70pzh',
-  dataset: 'production',
+  dataset: 'development',
   useCdn: true,
   token:
     'skv2l1LjkkEVi2Isec5kzzt0TKYKFSfg13OF8GG5NeSJ5Vw4nPJDbCIo4Sm9tjyqaf3MqTDOeHcVSVzi6dCsV9qPlqQv52elsPnZ4VL7gpLYtOa6m2JH6ju7juIkxjV3lprdar1wVtlFxtjOyFLvPGOpBdtS1GiAOBzI34aRMFSkixo6rrF9',
