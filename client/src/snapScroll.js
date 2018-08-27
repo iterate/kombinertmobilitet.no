690b54dbb0a17496bd3b2f89530c941b27c3b705
Arne Mæhlum <arne.mahlum@gmail.com>
2018-08-27 16:10:13 +0200

    Snap scroll: Snap to node in direction that user scrolled

--- client/src/snapScroll.js
+++ client/src/snapScroll.js
@@ -5,7 +5,17 @@ const ascending = (compareFn) => (one, two) => compareFn(one) - compareFn(two);
 const absScollOffset = (node) => Math.abs(node.getBoundingClientRect().y);
 const getKeyIn = object => value => Object.keys(object).find(key => object[key] === value);
 
-let timeout;
+const byScrollDirection = () => {
+  const scrollDiff = window.pageYOffset - scrollStart;
+
+  if (scrollDiff < 0) {
+    return node => node.getBoundingClientRect().y <= 0;
+  } else if (scrollDiff > 0) {
+    return node => node.getBoundingClientRect().y >= 0;
+  } else {
+    return () => true;
+  }
+}
 
 export function enable() {
   window.addEventListener('scroll', onScroll);
@@ -15,19 +25,28 @@ export function disable() {
   window.removeEventListener('scroll', onScroll);
 }
 
+let timeout;
+let scrollStart;
+
 function onScroll() {
   clearTimeout(timeout);
   timeout = setTimeout(onScrollEnd, 200);
+
+  if (scrollStart === void 0) {
+    scrollStart = window.pageYOffset;
+  }
 }
 
 function onScrollEnd() {
   scrollToClosest();
+  scrollStart = void 0;
 }
 
 function scrollToClosest() {
   Object.values(hashStore.nodeMap)
     .filter(Boolean)
     .map(ReactDOM.findDOMNode)
+    .filter(byScrollDirection())
     .sort(ascending(absScollOffset))
     .slice(0, 1)
     .forEach(closestPage => {
